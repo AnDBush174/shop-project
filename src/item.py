e7fa63b2d0582f7a0c42bf60916f41d571cf5145
Andrew <andbushuev@yandex.ru>
2023-09-12 14:54:38 +0500

    перенос нужных файлов для проверки

--- src/item.py
+++ src/item.py
@@ -1,74 +1,25 @@
-import csv
-import os
-
-
+# -*- coding: utf-8 -*-
+# src/item.py
 class Item:
-    """
-    Класс для представления товара в магазине.
-    """
-    pay_rate = 1.0
-    all = []
-    CSV_PATH = os.path.join(os.path.dirname(__file__), "items.csv")  # путь к csv-файлу
-
-    @classmethod
-    def instantiate_from_csv(cls):
-        """класс-метод, инициализирующий экземпляры класса Item данными из файла src/items.csv
-           открываем файл csv с помощью функции `open()` и создаем объект `csv.DictReader`,
-           который позволяет нам читать строки из файла csv в виде словарей."""
-        with open(cls.CSV_PATH) as file:
-            reader = csv.DictReader(file)
-            cls.all.clear()
-            for line in reader:
-                item = cls(line['name'], float(line['price']), int(line['quantity']))
-
-    @staticmethod
-    def string_to_number(string):
-        """Cтатический метод, возвращающий число из числа-строки"""
-        return int(float(string))
-
-    def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
-
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        self.quantity = quantity
+    def __init__(self, name, price, quantity):
+        self.name = name
         self.price = price
-        self.__name = name
-        self.all.append(self)
+        self.quantity = quantity
 
     def __repr__(self):
         return f"Item('{self.name}', {self.price}, {self.quantity})"
 
-    def __str__(self):
-        return self.name
+    def increase_quantity(self, increment):
+        self.quantity += increment
 
-    @property
-    def name(self):
-        """Возвращает имя"""
-        return self.__name
+    def decrease_quantity(self, decrement):
+        self.quantity -= decrement
 
-    @name.setter
-    def name(self, name):
-        """Обрезает имя, если оно больше 10 символов"""
-        if len(name) > 10:
-            self.__name = name[0:10]
-        else:
-            self.__name = name
-
-    def calculate_total_price(self) -> float:
-        """
-        Рассчитывает общую стоимость конкретного товара в магазине.
-
-        :return: Общая стоимость товара.
-        """
+    def calculate_total_price(self):
         return self.price * self.quantity
 
-    def apply_discount(self) -> None:
-        """
-        Применяет установленную скидку для конкретного товара.
-        """
-        self.price *= self.pay_rate
+    def __add__(self, other):
+        if isinstance(other, Item):
+            return self.quantity + other.quantity
 
+        raise TypeError("Нельзя складывать Item с экземпляром другого класса")
